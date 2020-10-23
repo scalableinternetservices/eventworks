dbf84ae94e51d4e5f3e8cba82a55cc09bf5380e4
pacbac <jclaytonc99@gmail.com>
2020-10-23 15:12:59 -0400

    Add database tables

---
+++ server/src/entities/Event.ts
@@ -0,0 +1,39 @@
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm'
+import { EventUserConfig } from './EventUserConfig'
+
+@Entity()
+export class Event extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  name: string
+
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string
+
+  @UpdateDateColumn()
+  timeCreated: Date
+
+  @UpdateDateColumn()
+  timeUpdated: Date
+
+  @Column('datetime')
+  startTime: Date
+
+  @Column('datetime')
+  endTime: Date
+
+  @Column('int')
+  capacity: number
+
+  // deal with this later
+  @Column({ default: false })
+  isRecurring: boolean
+
+  @OneToMany(() => EventUserConfig, eventUserConfig => eventUserConfig.event)
+  eventUserConfigs: EventUserConfig[]
+}
