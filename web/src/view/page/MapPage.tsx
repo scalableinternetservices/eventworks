5cc959acdde3388cbffb51d51dd8cd35b15fda9b
matttychen <matt@jstar.ai>
2020-12-09 04:03:40 -0800

    new nav bar for event page

--- web/src/view/page/MapPage.tsx
+++ web/src/view/page/MapPage.tsx
@@ -4,13 +4,25 @@ import * as React from 'react';
 import { useContext } from 'react';
 import { fetchEvent } from '../../graphql/fetchEvent';
 import { FetchEvent, FetchEventVariables } from '../../graphql/query.gen';
-import { H2 } from '../../style/header';
+import { H1, H2 } from '../../style/header';
 import { LoggedInUserCtx, UserContext } from '../auth/user';
 import { Room } from '../map/Room';
 import { AppRouteParams, Route } from '../nav/route';
 import { Page } from './Page';
 
 interface EventMapPageProps extends RouteComponentProps, AppRouteParams {}
+
+const navBar = {
+  width: "100vw",
+  height: "70px",
+  zIndex: 100,
+  background: "linear-gradient(90deg, #005587 0%, #2774AE 100%)",
+  opacity: "90%",
+  position: "absolute",
+  top: "0",
+  left: "0",
+  display: "flex"
+} as React.CSSProperties
 
 export function EventMapPage(props: EventMapPageProps) {
   const user = useContext(UserContext)
@@ -24,9 +36,7 @@ export function EventMapPage(props: EventMapPageProps) {
   }
 
   return (
-    <Page>
-      <MapPage eventId={eventId} user={user as LoggedInUserCtx} />
-    </Page>
+    <MapPage eventId={eventId} user={user as LoggedInUserCtx} />
   )
 }
 
@@ -67,8 +77,11 @@ export function MapPage({ user, eventId }: MapPageProps) {
   }
 
   return (
-    <Page>
-      <Room event={data} user={user} />
-    </Page>
+    <>
+    <div style={navBar}>
+      <H1 style={{color:"#fff", marginTop: "10", marginLeft: "50"}}>{data.event.name} hosted by {data.event.orgName}</H1>
+    </div>
+    <Room event={data} user={user} />
+    </>
   )
 }
